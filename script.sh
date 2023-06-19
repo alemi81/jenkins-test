b260d70540da029c2fc7d208bd9d1396fe924038
alemi81 <104173492+alemi81@users.noreply.github.com>
2023-06-19 11:07:31 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 echo "Here is the list of files"
 ls
 
-echo "Here is pwd enjoy jenkins enjoy jenkins enjoy jenkins enjoy jenkins "
+echo "Here is pwd enjoy jenkins enjoy jenkins enjoy the jenkins enjoy jenkins "
 pwd
