855c1a0b2c10be93b0fa88114339abd9997dd93b
alemi <alemighulam@yahoo.com>
2023-06-20 14:11:24 -0500

    new change

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 echo "Here is the list of files"
 ls
 touch melika.txt
-echo "Here is pwd enjoy jenkins enjoy jenkins enjoy the jenkins enjoy jenkins "
+echo "Here is pwd enjoy jenkins enjoy jenkins enjoy the jenkins enjoy jenkins enkins enjoy the jenkins enjoy jenkins enkins enjoy the jenkins enjoy jenkins enkins enjoy the jenkins enjoy jenkins "
 pwd
