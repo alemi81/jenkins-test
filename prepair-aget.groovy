4462060b9d80960bc4bd5e3576d96c768317b356
alemi <alemighulam@yahoo.com>
2023-06-20 15:29:59 -0500

    newr

---
+++ prepair-aget.groovy
@@ -0,0 +1,9 @@
+node {
+    stage("prepaire-agent"){
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSH_PRIVAE_KEY', usernameVariable: 'SSH_USER')]) {
+    // some block
+    sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVAE_KEY $SSH_USER@165.22.184.230 sudo apt install default-jre -y"
+    }
+            
+    }
+}
