476984292820a0611b6b1a881d20520a6bd1238c
alemi <alemighulam@yahoo.com>
2023-06-20 15:34:45 -0500

    git installer

--- prepair-aget.groovy
+++ prepair-aget.groovy
@@ -2,7 +2,7 @@ node {
     stage("prepaire-agent"){
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSH_PRIVAE_KEY', usernameVariable: 'SSH_USER')]) {
     // some block
-    sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVAE_KEY $SSH_USER@165.22.184.230 sudo apt install default-jre -y"
+    sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVAE_KEY $SSH_USER@165.22.184.230 sudo apt install default-jre -y && apt-get install git"
     }
             
     }
